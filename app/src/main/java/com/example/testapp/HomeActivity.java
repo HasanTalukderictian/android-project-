972b2ea28ac3111f3dfae17fd930bba262052cd6
= <hasantalukdercou@gmail.com>
2025-06-19 15:37:44 +0600

    BottomNavigation Added

--- app/src/main/java/com/example/testapp/HomeActivity.java
+++ app/src/main/java/com/example/testapp/HomeActivity.java
@@ -3,19 +3,58 @@ package com.example.testapp;
 import android.os.Bundle;
 import android.util.Log;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.example.testapp.databinding.ActivityHomeBinding;
 
 public class HomeActivity extends AppCompatActivity {
+
+    ActivityHomeBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        binding = ActivityHomeBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        replaceFragment(new HomeFragment());
+
         Log.d("HomeActivity", "onCreate: HomeActivity loaded");
 
-        setContentView(R.layout.activity_home);
+        // Load default fragment
+        // replaceFragment(new HomeFragment());
+
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+            int itemId = item.getItemId();
+
+            if (itemId == R.id.home) {
+
+                replaceFragment(new HomeFragment());
+                Log.d("BottomNav", "Home selected");
+                // replaceFragment(new HomeFragment());
+            } else if (itemId == R.id.profile) {
+
+                replaceFragment(new ProfileFragment());
+                Log.d("BottomNav", "Profile selected");
+                // replaceFragment(new ProfileFragment());
+            } else if (itemId == R.id.settings) {
+
+                replaceFragment(new SettingFragment());
+                Log.d("BottomNav", "Settings selected");
+                // replaceFragment(new SettingsFragment());
+            }
+
+            return true;
+        });
+    }
+
+    public void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.replace(R.id.frame_Layout, fragment);
+        transaction.commit();
     }
 }
